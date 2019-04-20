362c8e238abdfd517c603cdcf42c37234100cfc8
Dennis Dekker <info@dengsn.com>
2019-04-20 12:45:56 +0200

    Fixed incorrect oembed url

--- src/Soundcloud.php
+++ src/Soundcloud.php
@@ -9,24 +9,25 @@ class Soundcloud
 {
   // SoundCloud API location
   const URI = 'https://api.soundcloud.com/';
+  const OEMBED_URI = 'https://soundcloud.com/oembed';
   const AUTHORIZE_URI = 'https://soundcloud.com/connect';
-  
+
   // Variables
   private $clientId;
   private $clientSecret;
   private $redirectUri;
   private $accessToken;
   private $client;
-  
+
   // Constructor
   public function __construct(string $clientId, string $clientSecret = null, string $redirectUri = null)
   {
-    // initialize variables    
+    // initialize variables
     $this->clientId = $clientId;
     $this->clientSecret = $clientSecret;
     $this->redirectUri = $redirectUri;
     $this->accessToken = null;
-    
+
     // Initilalize HTTp client
     $this->client = new Client([
       'base_uri' => self::URI,
@@ -34,7 +35,7 @@ class Soundcloud
       'http_errors' => false
     ]);
   }
-  
+
   // Getters and setters
   public function getClientId(): string
   {
@@ -63,13 +64,13 @@ class Soundcloud
     $this->redirectUri = $redirectUri;
     return $this;
   }
-  
+
   // Get the access token
   public function getAccessToken()
   {
     return $this->accessToken;
   }
-  
+
   // Set the access token based on a /connect code
   public function authorizeWithCode($code): self
   {
@@ -78,9 +79,9 @@ class Soundcloud
       throw new InvalidArgumentException('No clientSecret was set');
     if ($this->redirectUri == null)
       throw new InvalidArgumentException('No redirectUri was set');
-      
+
     // Send the request
-    $json = $this->post('/oauth2/token',[
+    $json = $this->post('/oauth2/token', [
       'client_id' => $this->clientId,
       'client_secret' => $this->clientSecret,
       'redirect_uri' => $this->redirectUri,
@@ -90,11 +91,11 @@ class Soundcloud
 
     // Set the access token
     $this->accessToken = $json->access_token;
-      
+
     // Return self for chainability
     return $this;
   }
-  
+
   // Sets the access token based on user credentials
   public function authorizeWithCredentials(string $username, string $password): self
   {
@@ -103,9 +104,9 @@ class Soundcloud
       throw new InvalidArgumentException('No clientSecret was set');
     if ($this->redirectUri == null)
       throw new InvalidArgumentException('No redirectUri was set');
-    
+
     // Send the request
-    $json = $this->post('/oauth2/token',[
+    $json = $this->post('/oauth2/token', [
       'client_id' => $this->clientId,
       'client_secret' => $this->clientSecret,
       'redirect_uri' => $this->redirectUri,
@@ -113,10 +114,10 @@ class Soundcloud
       'username' => $username,
       'password' => $password
     ]);
-    
+
     // Set the access token
     $this->accessToken = $json->access_token;
-      
+
     // Return self for chainability
     return $this;
   }
@@ -130,21 +131,21 @@ class Soundcloud
       $query['client_id'] = $this->clientId;
       if ($this->accessToken)
         $query['oauth_token'] = $this->accessToken;
-      
-      // Send the request       
+
+      // Send the request
       if ($method == 'POST' || $method == 'PUT')
-        $response = $this->client->request($method,$uri,['query' => $query,'json' => $jsonBody]);
+        $response = $this->client->request($method, $uri, ['query' => $query,'json' => $jsonBody]);
       else
-        $response = $this->client->request($method,$uri,['query' => $query]);
-    
+        $response = $this->client->request($method, $uri, ['query' => $query]);
+
       // Check if authorization is needed
       if ($response->getStatusCode() == 401)
         throw new SoundcloudException("You must authorize your application first");
-    
+
       // Check if the request succeeded
       if (!preg_match('/[2-3][0-9]{2}/', $response->getStatusCode()))
-        throw new SoundcloudException("The request \"{$method} {$uri}\" returned with HTTP status code {$response->getStatusCode()}: {$response->getBody()}");
-    
+        throw new SoundcloudException("The request \"{$method} {$uri}" . (!empty($query) ? "?" . http_build_query($query) : "") . "\" returned with HTTP status code {$response->getStatusCode()}: {$response->getBody()}");
+
       // Otherwise handle the response
       if (strpos($response->getHeaderLine('Content-Type'),'application/json') === 0)
         return json_decode($response->getBody());
@@ -153,46 +154,46 @@ class Soundcloud
     }
     catch (RequestException $ex)
     {
-      throw new RuntimeException($ex->getMessage(),$ex->getCode(),$ex);
+      throw new RuntimeException($ex->getMessage(), $ex->getCode(), $ex);
     }
   }
-  
+
   // Sends a GET request
   public function get(string $uri, array $query = [])
   {
-    return $this->request('GET',$uri,$query);
+    return $this->request('GET', $uri, $query);
   }
 
   // Sends a POST request
   public function post(string $uri, array $body = [], array $query = [])
   {
-    return $this->request('POST',$uri,$query,$body);
+    return $this->request('POST', $uri, $query, $body);
   }
 
   // Sends a PUT request
   public function put(string $uri, array $body = [], array $query = [])
   {
-    return $this->request('PUT',$uri,$query,$body);
+    return $this->request('PUT', $uri, $query, $body);
   }
-  
+
   // Sends a DELETE request
   public function delete(string $uri, array $query = [])
   {
-    return $this->request('DELETE',$uri,$query);
+    return $this->request('DELETE', $uri, $query);
   }
-  
+
   // Sends a resolve request
   public function resolve(string $url)
   {
-    return $this->get('/resolve',['url' => $url]);
+    return $this->get('/resolve', ['url' => $url]);
   }
-  
+
   // Sends a oembed request
   public function oembed(string $url, array $query = [])
   {
-    return $this->get('/oembed',array_merge([
+    return $this->get(self::OEMBED_URI, array_merge([
       'url' => $url,
       'format' => 'json'
-    ],$query));
+    ], $query));
   }
 }
